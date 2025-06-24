26ea2b57edc9f7f60c68ce23950ba7da62a4fe04
Ryan Gonzalez <ryan.gonzalez@collabora.com>
2025-06-24 10:27:37 -0500

    chore: Switch vergen to vergen-gitcl
    vergen has been split into multiple helper crates, instead of using
    features, so we need to switch to the crate matching the desired
    behavior and update to the new API.
    
    Also update anyhow to continue to pass `direct-minimal-versions` checks.

--- gitlab-runner/build.rs
+++ gitlab-runner/build.rs
@@ -1,10 +1,9 @@
-use vergen::EmitBuilder;
+use vergen_gitcl::{Emitter, GitclBuilder};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Only emit git enabled variables if they're valid (in a git tree)
-    let _ = EmitBuilder::builder()
-        .git_dirty(true)
-        .git_sha(true)
+    let _ = Emitter::default()
+        .add_instructions(&GitclBuilder::default().dirty(true).sha(true).build()?)?
         .fail_on_error()
         .emit();
     Ok(())
